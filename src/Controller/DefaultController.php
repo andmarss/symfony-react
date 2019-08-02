dd12ab89f8dda17badce1fc68ce28cbe1b02c87c
Марков Андрей Игоревич <anrey-markov@mail.ru>
2019-08-03 01:27:26 +0300

    Routing parameters

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,13 +3,14 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/")
  */
-class DefaultController
+class DefaultController extends AbstractController
 {
     /**
      * @Route("/", name="default_index")
