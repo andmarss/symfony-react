6cbac4f2572e99719465ccdad37d06120dc7dcf8
Марков Андрей Игоревич <anrey-markov@mail.ru>
2019-08-09 02:22:25 +0300

    creating user and comment entity make many to one relation (and migration)

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -34,7 +34,8 @@ class BlogPost
     private $content;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="posts")
+     * @ORM\JoinColumn(nullable=false)
      */
     private $author;
     /**
@@ -83,18 +84,6 @@ class BlogPost
         return $this;
     }
 
-    public function getAuthor(): ?string
-    {
-        return $this->author;
-    }
-
-    public function setAuthor(string $author): self
-    {
-        $this->author = $author;
-
-        return $this;
-    }
-
     /**
      * @return mixed
      */
@@ -110,6 +99,25 @@ class BlogPost
     public function setSlug(string $slug): self
     {
         $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * @return User
+     */
+    public function getAuthor(): User
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param User $author
+     * @return BlogPost
+     */
+    public function setAuthor(User $author): self
+    {
+        $this->author = $author;
 
         return $this;
     }
