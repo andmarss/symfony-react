d10e663a9328993b0a4cdc38724dbb6235734518
Марков Андрей Игоревич <anrey-markov@mail.ru>
2019-08-09 22:16:09 +0300

    fixtures with references (for relations) password encoding in fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,13 +3,38 @@
 namespace App\DataFixtures;
 
 use App\Entity\BlogPost;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
+        $this->loadUsers($manager);
+
+        $this->loadPosts($manager);
+    }
+
+    public function loadPosts(ObjectManager $manager)
+    {
+        /**
+         * @var User $user
+         */
+        $user = $this->getReference('user_admin');
+
         /**
          * @var BlogPost $blogPost
          */
@@ -17,7 +42,7 @@ class AppFixtures extends Fixture
         $blogPost->setTitle('A first fixture post!');
         $blogPost->setPublished(new \DateTime('2019-08-07 12:00:00'));
         $blogPost->setContent('Post text');
-        $blogPost->setAuthor('Foo Bar');
+        $blogPost->setAuthor($user);
         $blogPost->setSlug('a-first-post');
 
         $manager->persist($blogPost);
@@ -29,11 +54,36 @@ class AppFixtures extends Fixture
         $blogPost->setTitle('A second fixture post!');
         $blogPost->setPublished(new \DateTime('2019-08-07 13:00:00'));
         $blogPost->setContent('Second post text');
-        $blogPost->setAuthor('Foo Bar');
+        $blogPost->setAuthor($user);
         $blogPost->setSlug('a-second-post');
 
         $manager->persist($blogPost);
 
+        $manager->flush();
+    }
+
+    public function loadComment(ObjectManager $manager)
+    {
+
+    }
+
+    public function loadUsers(ObjectManager $manager)
+    {
+        /**
+         * @var User $user
+         */
+        $user = new User();
+        $user->setUsername('admin');
+        $user->setEmail('admin@blog.com');
+        $user->setName('Foo Bar');
+
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user, 'secret'
+        ));
+
+        $this->addReference('user_admin', $user);
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
